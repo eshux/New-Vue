a354e49c17c2b7df0af39033b0f6c7797af2e487
eshux <54838488+eshux@users.noreply.github.com>
2023-11-15 17:52:56 +0200

    Form components - BaseSelect

---
+++ src/components/BaseSelect.vue
@@ -0,0 +1,41 @@
+<template>
+  <div>
+    <label v-if="label">{{ label }}</label>
+    <!-- Make sure input inherits the attributes with v-bind $attrs -->
+    <select @input="updateValue" :value="value" v-bind="$attrs">
+      <option
+        v-for="option in options"
+        :key="option"
+        :selected="option === value"
+      >
+        {{ option }}
+      </option>
+    </select>
+  </div>
+</template>
+
+<script>
+export default {
+  // by default attributes get inheritet by root element - in this case our div
+  // we need it to be the input element instead
+  inheritAttrs: false,
+  props: {
+    options: {
+      type: Array,
+      required: true
+    },
+    label: {
+      type: String,
+      default: ''
+    },
+    value: [String, Number]
+  },
+  methods: {
+    updateValue(event) {
+      this.$emit('input', event.target.value);
+    }
+  }
+};
+</script>
+
+<style scoped></style>
