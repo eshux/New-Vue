1c28fab44b5dedc8d370da1a3cf818c557342f8d
eshux <54838488+eshux@users.noreply.github.com>
2023-11-17 15:40:48 +0200

    Programmatic Navigation

--- src/views/event/CreateView.vue
+++ src/views/event/CreateView.vue
@@ -1,9 +1,19 @@
 <script setup>
-defineProps(['event']);
+import { useRouter } from 'vue-router';
+
+// const props = defineProps(['event']);
+const router = useRouter();
+
+const create = () => {
+  // If create API is successful
+  // Push back to event details page
+  router.push({ name: 'EventDetails' });
+};
 </script>
 
 <template>
   <div v-if="event">
     <p>Create Event</p>
+    <button @click="create">CREATE</button>
   </div>
 </template>
